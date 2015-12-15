06301ca9aee6ee592f43efa3d4be433c389155b8
Gaydukov Dmitriy <gaydukov@payqr.ru>
2015-12-15 13:32:50 +0300

    conf

---
+++ module/orm/PayqrModuleDbConfig.php
@@ -0,0 +1,10 @@
+<?php
+
+class PayqrModuleDbConfig
+{
+    public static $username = "";
+    public static $password = "";
+    public static $database = "";
+    public static $host = "";
+    public static $prefix = "";
+}
